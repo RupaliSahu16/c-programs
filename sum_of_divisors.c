3a469adbcb6c51c5f9691450c188eec8bf8c9778
Rupali Sahu <rrupalis1609@gmail.com>
2025-11-16 17:34:31 +0530

    Add files via upload

---
+++ sum_of_divisors.c
@@ -0,0 +1,15 @@
+//sum of divisors//
+
+#include <stdio.h>
+int main(){
+    int n,sum=0;
+    printf("enter the number:\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            sum=sum+i;
+        }
+    }
+    printf("Sum Of Divisor - %d",sum);
+    return 0;
+}
